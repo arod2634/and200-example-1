a6b1680484790b1bef45346185dd84d651aa0ce5
Alex Rodriguez <arod@integritystl.com>
2015-03-11 11:16:34 -0500

    Added a User class data model for storage of user object

---
+++ app/src/main/java/com/example1/arod/android200example1/User.java
@@ -0,0 +1,47 @@
+package com.example1.arod.android200example1;
+
+import java.io.Serializable;
+
+/**
+ * Created by arod on 3/10/15.
+ */
+public class User implements Serializable {
+
+    private String name;
+    private String petType;
+    private String spouseName;
+    private String numberOfKids;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPetType() {
+        return petType;
+    }
+
+    public void setPetType(String petType) {
+        this.petType = petType;
+    }
+
+    public String getSpouseName() {
+        return spouseName;
+    }
+
+    public void setSpouseName(String spouseName) {
+        this.spouseName = spouseName;
+    }
+
+    public String getNumberOfKids() {
+        return numberOfKids;
+    }
+
+    public void setNumberOfKids(String numberOfKids) {
+        this.numberOfKids = numberOfKids;
+    }
+
+}
